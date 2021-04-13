781bc50a31f17c715e151a107d30f8b6e721038a
George Harris <citizenharris@gmail.com>
2021-04-13 16:03:25 +0100

    Update RootMutation name

--- Getflix.Data.Domain/GraphQueryTypes/Mutations/RootMutation.cs
+++ Getflix.Data.Domain/GraphQueryTypes/Mutations/RootMutation.cs
@@ -9,7 +9,7 @@ namespace Getflix.Data.Domain.GraphQueryTypes.Mutations
     {
         public RootMutation()
         {
-            Name = "RootQuery";
+            Name = nameof(RootMutation);
             Field<SaveVideoMutation>("VideoMutations", resolve: context => new {});
             Field<SaveAudioMutation>("AudioMutations", resolve: context => new {});
             Field<SaveSubtitlesMutation>("SubtitlesMutations", resolve: context => new {});
